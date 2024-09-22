50ba96202eeb5ac0aa0fd889c5f5d71a135fe0fb
prince-nkuru <nkuruprinces@gmail.com>
2024-09-22 21:15:09 +0530

    added js footer

---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Footer.css'
+
+const Footer = () => {
+    return (
+        <footer>
+            <p>&copy; 2024 Online Pharmacy. All rights reserved.</p>
+        </footer>
+    );
+};
+
+export default Footer;
