43e7cbbf5ff289eacc5ac7dae02df015c5ac9ecb
prince-nkuru <nkuruprinces@gmail.com>
2024-09-24 21:11:04 +0530

    added about js

---
+++ src/components/About.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './About.css'
+
+const About = () => {
+    return (
+        <section id="about">
+            <h2>About Us</h2>
+            <p>We are committed to providing the best healthcare products and services.</p>
+        </section>
+    );
+};
+
+export default About;
