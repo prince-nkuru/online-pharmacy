3da687f1e98787aa07f8b612f61733fe03122dbf
prince-nkuru <nkuruprinces@gmail.com>
2024-09-30 22:41:27 +0530

    added hello

--- src/components/Hello.js
+++ src/components/Hello.js
@@ -1,12 +1,29 @@
 import React from 'react';
 import './Hello.css'
+import { useNavigate } from 'react-router';
+import pexel from './pexels-tima-miroshnichenko-7034125.jpg'
 
 const Hello = () => {
+  const navigate = useNavigate();
+  const handleShop = () => {
+    navigate('/products')
+  }
     return (
-        <section id="hero">
+        <section style={{
+          backgroundImage: `url(${pexel})`,
+          backgroundSize: 'cover', // Adjusts image to cover the whole div
+          backgroundPosition: 'center', // Centers the image
+          height: '100vh', // Full height
+          width: '100vw', // Full width
+          display: 'flex', // Optional: for centering content
+          flexDirection: 'column',
+          justifyContent: 'flex-start', // Optional
+          alignItems: 'center' // Optional
+        }} id="hero">
             <h2>Your Health, Our Priority</h2>
             <p>Browse a wide range of medications and health products.</p>
-            <a href="#products" className="btn">Shop Now</a>
+            <a href="#products" className="btn" onClick={handleShop}>Shop Now</a>
+            {/* <img src={pexel}/> */}
         </section>
     );
 };
