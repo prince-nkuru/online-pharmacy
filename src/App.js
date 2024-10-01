3c17da50981c7e7fc5fee9e49c29b4fbb002b21e
prince-nkuru <nkuruprinces@gmail.com>
2024-10-01 22:48:13 +0530

    added app js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
+import React from 'react';
+import Header from './components/Header';
+import Hello from './components/Hello';
+import ListProduct from './components/ListProduct';
+import About from './components/About';
+import Contact from './components/Contact';
+import Footer from './components/Footer';
 import './App.css';
+import Log from './components/Log';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+
+import { BrowserRouter, Routes, Route} from 'react-router-dom'
+
+
+const App = () => {
+    return (
+
+      <BrowserRouter>
+      <Header/>
+      
+      <Routes>
+      <Route path= '/products' element = {<ListProduct/>}></Route>
+      <Route path= '/about' element = {<About/>}></Route>
+      <Route path= '/contact' element = {<Contact/>}></Route>
+      <Route path= 'Home' element = {<Hello/>}></Route>
+      <Route path= 'log' element = {<Log/>}></Route>
+      </Routes>
+      {/* <Footer/> */}
+      </BrowserRouter>
+        // <div>
+        //     <Header />
+        //     <Hello />
+        //     <ListProduct />
+        //     <About />
+        //     <Contact />
+        //     <Log/>
+        //     <Footer />
+           
+            
+        // </div>
+    );
+};
 
 export default App;
