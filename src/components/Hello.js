01216e91fc1f1da1b940076f55dd010bfd3ac762
prince-nkuru <nkuruprinces@gmail.com>
2024-10-04 15:58:41 +0530

    added js

--- src/components/Hello.js
+++ src/components/Hello.js
@@ -1,33 +1,32 @@
-import React from 'react';
-import './Hello.css'
-import { useNavigate } from 'react-router';
+import React from "react";
+import "./Hello.css";
+import { useNavigate } from "react-router";
 
-import pexel from '../assets/pexels-tima-miroshnichenko-7034125.jpg'
+import pexel from "../assets/pexels-tima-miroshnichenko-7034125.jpg";
 
 const Hello = () => {
   const navigate = useNavigate();
   const handleShop = () => {
-    navigate('/products')
-  }
-    return (
-        <section style={{
-          backgroundImage: `url(${pexel})`,
-          backgroundSize: 'cover', // Adjusts image to cover the whole div
-          backgroundPosition: 'center', // Centers the image
-          height: '100vh', // Full height
-          width: '100vw', // Full width
-          display: 'flex', // Optional: for centering content
-          flexDirection: 'column',
-          justifyContent: 'flex-start', // Optional
-          alignItems: 'center' // Optional
-        }} id="hero">
-            <h1>Welcome to IBOZA Pharmacy</h1>
-            <h2>Your Health, Our Priority</h2>
-            <p>Browse a wide range of medications and health products.</p>
-            <a href="#products" className="btn" onClick={handleShop}>Shop Now</a>
-            {/* <img src={pexel}/> */}
-        </section>
-    );
+    navigate("/products");
+  };
+  return (
+    <section
+      style={{
+        backgroundImage: `url(${pexel})`,
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+      }}
+      id="hero"
+    >
+      <h1>Welcome to IBOZA Pharmacy</h1>
+      <h2>Your Health, Our Priority</h2>
+      <p>Browse a wide range of medications and health products.</p>
+      <a href="#products" className="btn" onClick={handleShop}>
+        Shop Now
+      </a>
+      {/* <img src={pexel}/> */}
+    </section>
+  );
 };
 
 export default Hello;
